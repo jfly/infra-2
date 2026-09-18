3eb60cf37a45d0260079353a8e663468f06206dd
Jeremy Fleischman <me@jfly.fyi>
2026-09-18 12:47:05 -0500

    Sort inputs

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "ngi-nix infrastructure";
 
+  inputs.buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
+  inputs.buildbot-nix.url = "github:nix-community/buildbot-nix";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
-  inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.sops-nix.url = "github:Mic92/sops-nix";
-  inputs.buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
-  inputs.buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.buildbot-nix.url = "github:nix-community/buildbot-nix";
   inputs.systems.url = "github:nix-systems/default-linux";
+  inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
 
   outputs =
     {
